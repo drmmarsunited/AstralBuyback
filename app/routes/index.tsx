2e7879295a8c3e1086cd0bfcfa770fd8a7930868
Mitch Robins <mtchelr@amazon.com>
2022-12-22 14:41:38 -0600

    Fixing value display to support locales

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -76,12 +76,12 @@ export default function Index() {
                     <dt className="text-md font-medium text-white">Buyback Value:</dt>
                     <dd className="mt-1 flex text-sm text-gray-400 sm:col-span-2 sm:mt-0">
                       <span className="flex-grow font-bold" id="value">
-                        {data ? `${data} ISK` : "Waiting for valid data..."}
+                        {data ? `${data.toLocaleString()} ISK` : "Waiting for valid data..."}
                         <button
                           className="px-2 focus:ring-indigo-500"
                           type="button"
                           title="Copy"
-                          onClick={() => navigator.clipboard.writeText(data?.toString() as string)}
+                          onClick={() => navigator.clipboard.writeText(data?.toLocaleString() as string)}
                         >
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                               <path fillRule="evenodd" d="M13.887 3.182c.396.037.79.08 1.183.128C16.194 3.45 17 4.414 17 5.517V16.75A2.25 2.25 0 0114.75 19h-9.5A2.25 2.25 0 013 16.75V5.517c0-1.103.806-2.068 1.93-2.207.393-.048.787-.09 1.183-.128A3.001 3.001 0 019 1h2c1.373 0 2.531.923 2.887 2.182zM7.5 4A1.5 1.5 0 019 2.5h2A1.5 1.5 0 0112.5 4v.5h-5V4z" clipRule="evenodd" />
